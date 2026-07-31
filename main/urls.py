6fa1d3d01fb0ed0e8936afb7d979fe71b1de398b
Sardar <sardorbeksattarov4@gmail.com>
2026-07-31 13:56:48 +0000

    Test-1

--- main/urls.py
+++ main/urls.py
@@ -19,10 +19,12 @@ from django.urls import path , include
 
 from django.conf import settings
 from django.conf.urls.static import static
-
+from myShop.views import product_list_view,category_list_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("book.urls")),
+    path('product_list_view/', product_list_view),
+    path('category_list_view/', category_list_view),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
